1b21122b57eb776e8c7004cc2f840970cc08ad8d
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-10-28 01:17:11 +0300

    Initial commit

---
+++ src/main/java/cashcard/CashCard/CashCardApplication.java
@@ -0,0 +1,14 @@
+package cashcard.CashCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashCardApplication.class, args);
+		System.out.println("Server is running");
+	}
+
+}

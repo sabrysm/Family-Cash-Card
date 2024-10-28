6e2e4f453a30200cfe1783d2871c89545bd1436f
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-10-28 08:33:19 +0300

    Added the following: - CashCard - CashCardController - CashCardApplicationTests - CashCardJsonTest

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,21 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+    @GetMapping("/{id}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long id) {
+        if (id.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}

f72062becf0ad5d8de0d7d0af15d156017a3ee9c
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-11-03 23:56:15 +0200

    added a new test for pagination

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,10 +1,15 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -33,5 +38,15 @@ class CashCardController {
     @GetMapping
     private ResponseEntity<Iterable<CashCard>> findAll() {
         return ResponseEntity.ok(cashCardRepository.findAll());
+    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize()
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
